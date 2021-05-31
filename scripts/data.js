e3982e295808508821969fea075e6d8406393442
Oana Popescu <oanapopescu93@gmail.com>
2021-05-31 12:19:23 +0300

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,29 @@
+var progress_element = [
+	{name: "January", value: 3250},
+    {name: "February", value: 3650},
+	{name: "March", value: 4400},
+	{name: "April", value: 3700},
+	{name: "May", value: 4000},
+	{name: "June", value: 4100},
+	{name: "July", value: 3250},
+	{name: "August", value: 3650},
+	{name: "September", value: 3700},
+	{name: "October", value: 4000},
+	{name: "November", value: 4100},
+	{name: "December", value: 3250},
+];
+
+var income_element = [
+	{name: "income01", value: 300},
+    {name: "income02", value: 140},
+    {name: "income03", value: 20},
+    {name: "income04", value: 120}
+];
+var expence_element = [
+	{name: "expence01", value: 100},
+    {name: "expence02", value: 140},
+    {name: "expence03", value: 20},
+    {name: "expence04", value: 120}
+];
+var mycolors_income = ["#fde23e","#f16e23", "#57d9ff","#937e88"];
+var mycolors_expence = ["#fde23e","#f16e23", "#57d9ff","#937e88"];
